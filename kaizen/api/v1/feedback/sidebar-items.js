c9b146d7a98f8bd0915ec44bf534a9ed1c3af6d5
realaravinth <realaravinth@batsense.net>
2021-09-17 07:13:32 +0000

    Deploying to gh-pages from  @ 6cc174fc484a73556b63d37b02cddbdd7096569d 🚀

--- kaizen/api/v1/feedback/sidebar-items.js
+++ kaizen/api/v1/feedback/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["services",""]],"mod":[["routes",""]],"struct":[["DescriptionReq",""],["RatingReq",""],["RatingResp",""],["description",""],["rating",""]]});
+initSidebarItems({"constant":[["URL_MAX_LENGTH",""]],"fn":[["services",""]],"mod":[["routes",""]],"struct":[["DescriptionReq",""],["RatingReq",""],["RatingResp",""],["description",""],["rating",""]]});
